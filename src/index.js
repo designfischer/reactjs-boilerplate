466af5fe2f69dfdb5fe3098c8edcdafb5a355f9d
Rafael Fischer <47259718+designfischer@users.noreply.github.com>
2020-08-17 23:21:30 -0300

    apollo tutorial
    first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+})
+
+async function getData() {
+  try {
+    const result = await client.query({
+      query: gql`
+        query GetRates {
+          rates(currency: "USD") {
+            currency
+          }
+        }
+      `
+    })
+    console.log(result)
+  } catch(err) {
+    alert(err)
+  }  
+}
+
+getData()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
